14f16692970625281a6b511724e3d863dd86a38e
Arlana(Zhanbota) Dyussebayeva <arlanakznl@gmail.com>
2021-05-17 01:30:23 +0600

    ADD: dispatched "movies" from db

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import ImgSlider from './ImgSlider';
 import Viewers from './Viewers';
 import Movies from './Movies';
+import db from '../firebase';
+import { useDispatch } from 'react-redux';
+import { setMovies } from '../features/movie/movieSlice';
 
 function Home() {
+    const dispatch = useDispatch();
+    //useEffect is run once Home component is loaded
+    useEffect( () => {
+        db
+        .collection("movies")
+        .onSnapshot( (snapshot) => {
+            let tempMovies = snapshot.docs.map((doc) => {
+                // console.log(doc.data())
+                return {
+                    id: doc.id, ...doc.data()
+                }
+            })
+            dispatch(setMovies(tempMovies));
+        })
+    }, [])
+
     return (
         <Container>
             <ImgSlider />
